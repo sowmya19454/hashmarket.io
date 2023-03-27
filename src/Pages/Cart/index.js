95ba27c50f1ec172b00dc46fcf54f20f4d358fa3
sowmya <sowmya19454@gmail.com>
2023-03-27 20:18:04 +0530

    cart function implimentation

--- src/Pages/Cart/index.js
+++ src/Pages/Cart/index.js
@@ -37,7 +37,67 @@ const Cart = () => {
           </div>
         </div>
       )}
-      // Your code goes here
+       {items.length > 0 && (
+        <div className="flex flex-wrap max-w-7xl mx-auto my-4">
+          <div className="flex flex-col flex-1">
+            {items.map((item) => {
+              return (
+                <div
+                  className="w-full sm:w-2/2 md:w-2/2 xl:w-5/5 p-4 my-auto"
+                  key={item.id}
+                >
+                  <div className={styles.bgCart}>
+                    <div className="flex flex-row h-48">
+                      <img
+                        className="w-32 my-auto p-4 object-contain"
+                        src={item.image}
+                        alt="Cart Item"
+                      />
+                      <div className="flex flex-col ml-2 mt-2">
+                        <Link to={`/product/${item.id}`}>
+                          <h2 className="text-sm title-font text-yellow-400 tracking-widest hover:text-blue-600 mt-2">
+                            Brand
+                          </h2>
+                          <p className="font-extralight">{item.title}</p>
+                        </Link>
+                        <p className="mt-auto mb-4 text-yellow-400 font-extralight text-xl">
+                          $ {item.price}
+                        </p>
+                      </div>
+                      <div className="flex flex-row ml-auto">
+                        <button className="w-5 h-5 ml-auto m-4 hover:text-red-500" onClick={() => removeFromCart(item.id)}>
+                          <TrashIcon />
+                        </button>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              );
+            })}
+          </div>
+          <div className="w-full h-full sm:w-2/2 md:w-2/2 xl:w-1/5 p-4">
+            <div className={styles.bgCart}>
+              <div className="flex flex-col p-4">
+                <span className="text-xl mb-4 font-semibold">
+                  Order Summary
+                </span>
+                <span className="text-sm my-2 font-extralight flex">
+                  Subtotal <span className="ml-auto font-normal">$ {subtotal}</span>
+                </span>
+                <span className="text-sm my-2 font-extralight flex">
+                  Shipping Estimate <span className="ml-auto font-normal">$ 5</span>
+                </span>
+                <span className="text-sm my-2 font-extralight flex">
+                  Tax Estimate <span className="ml-auto font-normal">$ 5</span>
+                </span>
+                <span className="text-md my-2 font-normal flex">
+                  Order Total <span className="ml-auto">$ {parseFloat(subtotal) + 10}</span>
+                </span>
+              </div>
+            </div>
+          </div>
+        </div>
+      )}
     </div>
   );
 };
